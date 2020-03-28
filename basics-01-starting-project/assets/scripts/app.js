00afb8ce71d81fe10e724426fa79e7be786468a7
Sanjay <sanjaykviswas@gmail.com>
2020-03-28 19:07:37 +0530

    After function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,8 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-let calculationDescription = `( ${defaultResult} + 10) * 3) / 2 - 1`;
-outputResult(currentResult, calculationDescription);
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, "");
+}
+
+addBtn.addEventListener("click", add);
