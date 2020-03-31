c5826b447c45a8db9c953d77b8bfa0a2e6d2b49b
Sanjay <sanjaykviswas@gmail.com>
2020-03-31 21:47:41 +0530

    After bind

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,4 +1,9 @@
 const defaultResult = 0;
+const CALCULATION_TYPE_ADD = "ADD";
+const CALCULATION_TYPE_SUBTRACT = "SUBTRACT";
+const CALCULATION_TYPE_MULTIPLY = "MULTIPLY";
+const CALCULATION_TYPE_DIVIDE = "DIVIDE";
+
 let currentResult = defaultResult;
 let logEntries = [];
 
@@ -35,24 +40,24 @@ function calculateResult(calculationType) {
   const enteredInput = getUserNumberInput();
   let mathOperator;
   if (
-    (calculationType !== "ADD" &&
-      calculationType !== "SUBTRACT" &&
-      calculationType !== "MULTIPLY" &&
-      calculationType !== "DIVIDE") ||
+    (calculationType !== CALCULATION_TYPE_ADD &&
+      calculationType !== CALCULATION_TYPE_SUBTRACT &&
+      calculationType !== CALCULATION_TYPE_MULTIPLY &&
+      calculationType !== CALCULATION_TYPE_DIVIDE) ||
     !enteredInput
   ) {
     return;
   }
-  if (calculationType === "ADD") {
+  if (calculationType === CALCULATION_TYPE_ADD) {
     currentResult += enteredInput;
     mathOperator = "+";
-  } else if (calculationType === "SUBTRACT") {
+  } else if (calculationType === CALCULATION_TYPE_SUBTRACT) {
     currentResult -= enteredInput;
     mathOperator = "-";
-  } else if (calculationType === "MULTIPLY") {
+  } else if (calculationType === CALCULATION_TYPE_MULTIPLY) {
     currentResult *= enteredInput;
     mathOperator = "*";
-  } else if (calculationType === "DIVIDE") {
+  } else if (calculationType === CALCULATION_TYPE_DIVIDE) {
     currentResult /= enteredInput;
     mathOperator = "/";
   }
@@ -62,26 +67,43 @@ function calculateResult(calculationType) {
 
 // For adding and displaying result
 function add() {
-  calculateResult("ADD");
+  calculateResult(CALCULATION_TYPE_ADD);
 }
 
 // For substracting and displaying result
 function subtract() {
-  calculateResult("SUBTRACT");
+  calculateResult(CALCULATION_TYPE_SUBTRACT);
 }
 
 // For multiplying and displaying result
 function multiply() {
-  calculateResult("MULTIPLY");
+  calculateResult(CALCULATION_TYPE_MULTIPLY);
 }
 
 // For dividing and displaying result
 function divide() {
-  calculateResult("DIVIDE");
+  calculateResult(CALCULATION_TYPE_DIVIDE);
 }
 
 // For listening the event from buttons
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+// addBtn.addEventListener("click", add);
+// subtractBtn.addEventListener("click", subtract);
+// multiplyBtn.addEventListener("click", multiply);
+// divideBtn.addEventListener("click", divide);
+
+addBtn.addEventListener(
+  "click",
+  calculateResult.bind(this, CALCULATION_TYPE_ADD)
+);
+subtractBtn.addEventListener(
+  "click",
+  calculateResult.bind(this, CALCULATION_TYPE_SUBTRACT)
+);
+multiplyBtn.addEventListener(
+  "click",
+  calculateResult.bind(this, CALCULATION_TYPE_MULTIPLY)
+);
+divideBtn.addEventListener(
+  "click",
+  calculateResult.bind(this, CALCULATION_TYPE_DIVIDE)
+);
